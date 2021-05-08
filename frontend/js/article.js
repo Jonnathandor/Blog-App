c642232df78176487b02ab77b5c5cf8225ed2dc0
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-05-08 17:20:51 -0500

    added mono repo structure

---
+++ frontend/js/article.js
@@ -0,0 +1,14 @@
+const API_URL = "http://localhost:3000/api/articles/";
+const API_BASE_URL = "http://localhost:3000/";
+
+window.onload = () => {
+    getArticle();
+}
+
+const getArticle = () => {
+    
+}
+
+const buildArticle = (data) => {
+    
+}
---
+++ frontend/js/index.js
@@ -0,0 +1,13 @@
+const API_URL = "http://localhost:3000/api/articles";
+const API_BASE_URL = "http://localhost:3000/";
+
+window.onload = () => {
+    getPosts();
+}
+
+const getArticles = () => {
+}
+
+const buildArticles = (blogArticles) => {
+
+}
