cac6b588512176a7ea528c6d817fefb28e868f2f
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-05-09 19:14:32 -0500

    added the API and connected the home page to get articles route

---
+++ backend/api/models/article.js
@@ -0,0 +1,33 @@
+//const articles  = require('../../data.json')
+const PATH = './data.json';
+const fs = require('fs');
+
+class Article {
+    getArticles(){
+        return this.readArticles();
+    }
+
+    getArticle(articleId){
+        const articles = this.readArticles();
+        let article = articles.filter(article => article.id == articleId)
+        return article;
+    }
+
+    addArticle(newArticle){
+        const currentArticles = this.readArticles();
+        currentArticles.unshift(newArticle)
+        this.saveArticle(currentArticles)
+    }
+
+    readArticles(){
+        //return articles;
+        return JSON.parse(fs.readFileSync(PATH));
+    }
+
+    saveArticle(article){
+        article = JSON.stringify(article);
+        fs.writeFileSync(PATH, article);
+    }
+}
+
+module.exports = Article
---
+++ backend/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const Article = require('./api/models/article');
+const article = new Article();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+})
+
+// This middleware makes the uploads folders public
+app.use('/uploads', express.static('uploads'));
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World');
+})
+
+app.get('/api/articles', (req, res) => {
+    res.status(200).send(article.getArticles());
+})
+
+app.get('/api/article/:article_id', (req, res) => {
+    let found = article.getArticle(req.params.article_id);
+
+    if(found.length !== 0){
+        res.status(200).send(found);
+    } else {
+        res.status(404).send("Not Found");
+    }
+    
+})
+
+app.post('/api/articles', (req, res) => {
+    
+})
+
+app.listen(3000, () => {
+    console.log('Listening on http://localhost:3000');
+})
